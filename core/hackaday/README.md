dfb2b5b503926567ce322d1e07b91989cade3c51
Elliot Williams <elliot@pre.postero.us>
2017-03-30 22:45:23 +0200

    added the all-important login screen frills
    you know you wanted a Jolly Wrencher!

---
+++ core/hackaday/README.md
@@ -0,0 +1,1 @@
+Files, essential and otherwise, that make up the Hackaday contribution to the system.  All of them are included in the `mecrisp-stellaris-Hackaday.bin` file.
---
+++ core/hackaday/wrencher.fs
@@ -0,0 +1,42 @@
+
+( Best viewed in white on black )
+: wrencher-16 cr 
+."      .Xx                    xX.    " cr 
+."       .@X.                .X@.     " cr 
+."   x   xXXX                XXXx   x " cr 
+."   .X@XXXXX@.            .@XXXXX@X. " cr 
+."     .xxxXXXXx.x@XXXX@x.xXXXXxxx.   " cr 
+."          xX.@XXXXXXXXXX@.Xx        " cr 
+."            @XXXXXXXXXXXX@          " cr 
+."           .X@.  xXXx  .@X.         " cr 
+."           xX.  .@XX@.  .Xx         " cr 
+."           .XXxXXXxxXXXxXX.         " cr 
+."          xx.XXXXXxxXXXXX.xx        " cr 
+."     .xxxXXX@.@XXXXXXXX@.@XXXxxx.   " cr 
+."   .XxXXXXX@. .@@.@@.@@. .@XXXXXxX. " cr 
+."   .   xXXX                XXXx   . " cr 
+."       .@X.                .X@.     " cr 
+."      xXx                    xXx    " cr 
+cr ;
+
+( Should work fine on either )
+: wrencher-12 cr
+."    `[-.               ,-]'" cr
+." ,  ,'  |             |  `.  ," cr
+." \'-    `.    ___    ,'    -'|" cr
+."  `....   I>-'   `-<L   ,..,-'" cr
+."       `.|'         `|,'" cr
+."        .' _,_   _._ '." cr
+."        | |  _| `.  | |" cr
+."        | `.'  ,  `.' |" cr
+."       ,-`    |'|    /-." cr
+."  ,---'  `|.       , '  `---." cr
+." /,._   ,' \.,\_,,,' -.    ..\" cr
+." '   \  /             \  /'  '" cr
+."    /.,'               `.:.   " cr
+cr ;
+
+( Here's how to integrate it into the startup 
+: init ."   =<[ Hackaday Edition ]>=" wrencher-12 ;
+)
+
